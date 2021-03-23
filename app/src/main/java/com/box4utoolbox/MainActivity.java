adff4894f3381b13ddc5c55881578d1c6f5e43f4
Barmanek <gasiorek.kacper@gmail.com>
2021-03-23 22:34:23 +0100

    added grid template

--- app/src/main/java/com/box4utoolbox/MainActivity.java
+++ app/src/main/java/com/box4utoolbox/MainActivity.java
@@ -2,6 +2,7 @@ package com.box4utoolbox;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.GridView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -41,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
     }
 
     @Override
--- app/src/main/java/com/box4utoolbox/ui/galeria/GaleriaFragment.java
+++ app/src/main/java/com/box4utoolbox/ui/galeria/GaleriaFragment.java
@@ -7,10 +7,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.box4utoolbox.R;
 
@@ -19,7 +16,6 @@ public class GaleriaFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_galeria, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
         return root;
     }
 }
